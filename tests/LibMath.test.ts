a01185ca43f26e663d6ac59728f79779c12a18e5
zhutianchi <zhutianchi@mcarlo.com>
2020-10-27 12:23:01 +0800

    tests

--- tests/LibMath.test.ts
+++ tests/LibMath.test.ts
@@ -13,15 +13,46 @@ describe('LibMath', () => {
   });
 
   describe('mostSignificantBit', () => {
+    it('normal', async () => {
+      expect(await libMath.mostSignificantBit('0')).to.equal('0');
+      expect(await libMath.mostSignificantBit('1')).to.equal('0');
+      expect(await libMath.mostSignificantBit('2')).to.equal('1');
+      expect(await libMath.mostSignificantBit('3')).to.equal('1');
+      expect(await libMath.mostSignificantBit('4')).to.equal('2');
+      expect(await libMath.mostSignificantBit('7')).to.equal('2');
+      expect(await libMath.mostSignificantBit('8')).to.equal('3');
+    });
   });
 
-  it('Assigns initial balance', async () => {
-    expect(await libMath.mostSignificantBit('0')).to.equal('0');
-    expect(await libMath.mostSignificantBit('1')).to.equal('0');
-    expect(await libMath.mostSignificantBit('2')).to.equal('1');
-    expect(await libMath.mostSignificantBit('3')).to.equal('1');
-    expect(await libMath.mostSignificantBit('4')).to.equal('2');
-    expect(await libMath.mostSignificantBit('7')).to.equal('2');
-    expect(await libMath.mostSignificantBit('8')).to.equal('3');
+  describe('sqrt', () => {
+    it('small', async () => {
+      expect(await libMath.sqrt('0')).to.equal('0');
+      expect(await libMath.sqrt('1')).to.equal('1');
+      expect(await libMath.sqrt('2')).to.equal('1');
+      expect(await libMath.sqrt('3')).to.equal('1');
+      expect(await libMath.sqrt('4')).to.equal('2');
+      expect(await libMath.sqrt('5')).to.equal('2');
+      expect(await libMath.sqrt('6')).to.equal('2');
+      expect(await libMath.sqrt('7')).to.equal('2');
+      expect(await libMath.sqrt('8')).to.equal('2');
+      expect(await libMath.sqrt('9')).to.equal('3');
+    });
+
+    it('normal', async () => {
+      // 1e-9
+      expect(await libMath.sqrt('1000000000000000000')).to.equal('1000000000');
+
+      // 1
+      expect(await libMath.mostSignificantBit('1000000000000000000000000000000000000')).to.equal('1000000000000000000');
+
+      // 0.35
+      expect(await libMath.mostSignificantBit('122500000000000000000000000000000000')).to.equal('350000000000000000');
+
+      // 7e4
+      expect(await libMath.mostSignificantBit('4900000000000000000000000000000000000000000000')).to.equal('70000000000000000000000');
+
+      // 1e12
+      expect(await libMath.mostSignificantBit('1000000000000000000000000000000000000000000000000000000000000')).to.equal('1000000000000000000000000000000');
+    });
   });
 });
