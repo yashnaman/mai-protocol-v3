788831596893c80a0e5a4b7107f400e096472dee
zhutianchi <zhutianchi@mcarlo.com>
2020-10-27 14:23:54 +0800

    missing func name

--- contracts/tests/TestMath.sol
+++ contracts/tests/TestMath.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.7.1;
+pragma solidity >=0.7.4;
 
 import "../libs/LibMath.sol";
 
--- test/LibMath.test.ts
+++ test/LibMath.test.ts
@@ -1,10 +1,11 @@
-import chai from "chai";
-import {Contract} from 'ethers';
-import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import { expect, use } from "chai";
+import { waffleChai } from "@ethereum-waffle/chai";
+import { Contract } from 'ethers';
+import { deployContract, MockProvider, solidity } from 'ethereum-waffle';
 import LibMath from '../artifacts/TestLibMath.json';
-chai.use(solidity);
 
-const { expect } = chai;
+use(solidity);
+use(waffleChai);
 
 describe('LibMath', () => {
   const [wallet, walletTo] = new MockProvider().getWallets();
@@ -16,13 +17,13 @@ describe('LibMath', () => {
 
   describe('mostSignificantBit', () => {
     it('normal', async () => {
-      expect(await libMath.mostSignificantBit('0')).to.equal('0');
-      expect(await libMath.mostSignificantBit('1')).to.equal('0');
-      expect(await libMath.mostSignificantBit('2')).to.equal('1');
-      expect(await libMath.mostSignificantBit('3')).to.equal('1');
-      expect(await libMath.mostSignificantBit('4')).to.equal('2');
-      expect(await libMath.mostSignificantBit('7')).to.equal('2');
-      expect(await libMath.mostSignificantBit('8')).to.equal('3');
+      expect(await libMath.mostSignificantBit('0')).to.equal(0);
+      expect(await libMath.mostSignificantBit('1')).to.equal(0);
+      expect(await libMath.mostSignificantBit('2')).to.equal(1);
+      expect(await libMath.mostSignificantBit('3')).to.equal(1);
+      expect(await libMath.mostSignificantBit('4')).to.equal(2);
+      expect(await libMath.mostSignificantBit('7')).to.equal(2);
+      expect(await libMath.mostSignificantBit('8')).to.equal(3);
     });
   });
 
@@ -43,18 +44,18 @@ describe('LibMath', () => {
     it('normal', async () => {
       // 1e-9
       expect(await libMath.sqrt('1000000000000000000')).to.equal('1000000000');
-
+      
       // 1
-      expect(await libMath.mostSignificantBit('1000000000000000000000000000000000000')).to.equal('1000000000000000000');
+      expect(await libMath.sqrt('1000000000000000000000000000000000000')).to.equal('1000000000000000000');
 
       // 0.35
-      expect(await libMath.mostSignificantBit('122500000000000000000000000000000000')).to.equal('350000000000000000');
+      expect(await libMath.sqrt('122500000000000000000000000000000000')).to.equal('350000000000000000');
 
       // 7e4
-      expect(await libMath.mostSignificantBit('4900000000000000000000000000000000000000000000')).to.equal('70000000000000000000000');
+      expect(await libMath.sqrt('4900000000000000000000000000000000000000000000')).to.equal('70000000000000000000000');
 
       // 1e12
-      expect(await libMath.mostSignificantBit('1000000000000000000000000000000000000000000000000000000000000')).to.equal('1000000000000000000000000000000');
+      expect(await libMath.sqrt('1000000000000000000000000000000000000000000000000000000000000')).to.equal('1000000000000000000000000000000');
     });
   });
 });
