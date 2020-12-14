1d5c27110b8b0462aceba7a9ca183e9f6407cb40
yangzhao <yangzhao@mcarlo.com>
2020-12-14 18:47:57 +0800

    aupdate

--- contracts/test/TestShareToken.sol
+++ contracts/test/TestShareToken.sol
@@ -4,7 +4,7 @@ pragma solidity 0.7.4;
 import "../token/ShareToken.sol";
 
 contract TestShareToken is ShareToken {
-    function setDefaultAdmin(address admin) public {
-        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+    function setAdmin(address admin) public {
+        _setupRole(ADMIN_ROLE, admin);
     }
 }
