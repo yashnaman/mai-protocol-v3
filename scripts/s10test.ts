b5bf57629a3f8703bf8ad40b8b791083070f2481
zhutianchi <zhutianchi@mcarlo.com>
2020-12-16 19:22:13 +0800

    default beta

--- scripts/s10test.ts
+++ scripts/s10test.ts
@@ -71,13 +71,13 @@ async function set1(accounts: any[], poolCreator, weth) {
     const mtx1 = await liquidityPool.createMarket("0xF34BA0c3c81C88867195143B4368f1cA36AD2571",
         // imr          mmr            operatorfr        lpfr             rebate        penalty        keeper       insur
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.01"), toWei("0.00015"), toWei("0.0001"), toWei("0.005"), toWei("3")],
+        [toWei("0.01"), toWei("0.0001"), toWei("0.000066"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
     const mtx2 = await liquidityPool.createMarket("0xDe1421E459E9799e8CeCDd57069329E9ca3ebB82",
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.01"), toWei("0.00015"), toWei("0.0001"), toWei("0.005"), toWei("3")],
+        [toWei("0.01"), toWei("2"), toWei("1.33"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
@@ -113,25 +113,25 @@ async function set2(accounts: any[], poolCreator, weth) {
 
     const mtx1 = await liquidityPool.createMarket("0x2dccA2b995651158Fe129Ddd23D658410CEa8254",
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.001"), toWei("24"), toWei("16"), toWei("0.005"), toWei("3")],
+        [toWei("0.001"), toWei("36"), toWei("24"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
     const mtx2 = await liquidityPool.createMarket("0x90aa806A0a2743991CC05aE2206b7d06d6FDbdc4",
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.001"), toWei("24"), toWei("16"), toWei("0.005"), toWei("3")],
+        [toWei("0.001"), toWei("1200"), toWei("800"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
     const mtx3 = await liquidityPool.createMarket("0xD9C29A2FbC360cf673dcDB65A87B101f6FD10DEA",
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.001"), toWei("24"), toWei("16"), toWei("0.005"), toWei("3")],
+        [toWei("0.001"), toWei("6"), toWei("4"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
     const mtx4 = await liquidityPool.createMarket("0x37398F5C3D11c11386294Dd3e7464717a10Ffb15",
         [toWei("0.05"), toWei("0.02"), toWei("0.00005"), toWei("0.0005"), toWei("0.5"), toWei("0.01"), toWei("0.1"), toWei("0.2")],
-        [toWei("0.001"), toWei("24"), toWei("16"), toWei("0.005"), toWei("3")],
+        [toWei("0.001"), toWei("192"), toWei("128"), toWei("0.005"), toWei("3")],
         [toWei("0"), toWei("0"), toWei("0"), toWei("0"), toWei("0")],
         [toWei("1"), toWei("20000"), toWei("20000"), toWei("1"), toWei("10")],
     )
