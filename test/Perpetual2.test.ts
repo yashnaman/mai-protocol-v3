0f52aff1337b75bc6acb54a263e6dd843ac22d6e
zhiji666 <zhiji@mcarlo.com>
2021-04-29 16:43:21 +0800

    update test

--- test/Perpetual2.test.ts
+++ test/Perpetual2.test.ts
@@ -332,8 +332,7 @@ describe('Perpetual2', () => {
             await liquidityPool.clear(0);
             await liquidityPool.connect(user1).settle(0, user1.address, true);
             // const info = await liquidityPool.getLiquidityPoolInfo();
-            // already no collateral in pool
-            await expect(liquidityPool.connect(user2).removeLiquidity(await stk.balanceOf(user2.address), 0, true)).to.be.revertedWith("pool margin must be positive");
+            await liquidityPool.connect(user2).removeLiquidity(await stk.balanceOf(user2.address), 0, true);
 
             // console.log(fromWei(await ctk.balanceOf(user1.address)));
             // console.log(fromWei(await ctk.balanceOf(liquidityPool.address)));
