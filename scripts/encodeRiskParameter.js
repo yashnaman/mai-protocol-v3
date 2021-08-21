decba03f4262de74bc33505c0bbf9d3d0027214e
zhutianchi <zhutianchi@mcarlo.com>
2021-08-21 19:37:52 +0800

    add a tool to encode propose

--- scripts/encodeRiskParameter.js
+++ scripts/encodeRiskParameter.js
@@ -7,7 +7,13 @@ function printNumberArray(arr) {
 
 const OperatorProxy = new ethers.utils.Interface([
   'function updatePerpetualRiskParameter(address liquidityPool, uint256 perpetualIndex, int256[8] calldata riskParams)',
+  'function propose(address liquidityPool, string[] calldata signatures, bytes[] calldata calldatas, string calldata description) external returns (uint256)',
+  'function proposeToUpgradeAndCall(address liquidityPool, bytes32 targetVersionKey, bytes calldata dataForLiquidityPool, bytes calldata dataForGovernor, string calldata description) external returns (uint256)',
   'function createPerpetual(address liquidityPool, address oracle, int256[9] calldata baseParams, int256[8] calldata riskParams, int256[8] calldata minRiskParamValues, int256[8] calldata maxRiskParamValues)',
+])
+const LiquidityPool = new ethers.utils.Interface([
+  'function createPerpetual(address oracle, int256[9] calldata baseParams, int256[8] calldata riskParams, int256[8] calldata minRiskParamValues, int256[8] calldata maxRiskParamValues)',
+  'function propose(string[] calldata signatures, bytes[] calldata calldatas, string calldata description)',
 ])
 
 // ===============================================================================================
@@ -27,12 +33,52 @@ const OperatorProxy = new ethers.utils.Interface([
 //   ])
 // )
 // ===============================================================================================
-//                                    createPerpetual
+//                                    vote - createPerpetual
+// ===============================================================================================
+// console.log(
+//   OperatorProxy.encodeFunctionData("propose", [
+//     '0x80918a9bb46bc1c2a03d3a9e09432ef4ee0bb048', // just test pool
+//     [
+//       // signatures
+//       "createPerpetual(address,int256[9],int256[8],int256[8],int256[8])",
+//     ],
+//     // calldatas
+//     [
+//       '0x' + LiquidityPool.encodeFunctionData("createPerpetual", [
+//         '0x6ee936BdBD329063E8CE1d13F42eFEf912E85221', // BTC oracle
+//         // imr          mmr            operatorfr        lpfr              rebate        penalty        keeper       insur         oi
+//         [toWei("0.04"), toWei("0.03"), toWei("0.00010"), toWei("0.00055"), toWei("0.2"), toWei("0.01"), toWei("10"), toWei("0.5"), toWei("3")],
+//         // alpha           beta1            beta2             frLimit        lev         maxClose       frFactor        defaultLev
+//         [toWei("0.00075"), toWei("0.0075"), toWei("0.00525"), toWei("0.01"), toWei("1"), toWei("0.05"), toWei("0.005"), toWei("10")],
+//         [toWei("0"),       toWei("0"),      toWei("0"),       toWei("0"),    toWei("0"), toWei("0"),    toWei("0"),     toWei("0")],
+//         [toWei("0.1"),     toWei("0.5"),    toWei("0.5"),     toWei("0.1"),  toWei("5"), toWei("1"),    toWei("0.1"),   toWei("10000000")]    
+//       ]).slice(2 + 8), // skip the function signature
+//     ],
+//     JSON.stringify({
+//       underlyingSymbol: "BTC",
+//       collateralSymbol: "USDC",
+//     }),
+//   ])
+// )
+// ===============================================================================================
+//                                     vote - upgrade
+// ===============================================================================================
+// console.log(
+//   OperatorProxy.encodeFunctionData("proposeToUpgradeAndCall", [
+//     '0x80918a9bb46bc1c2a03d3a9e09432ef4ee0bb048', // just test pool
+//     '0x58179930b25ca567ea71c55342e0ed9d4a3052109212ac65b2ae0832b1f18772', // versionKey
+//     '0x', // dataForLiquidityPool
+//     '0x', // dataForGovernor
+//     JSON.stringify({}), // description
+//   ])
+// )
+// ===============================================================================================
+//                     createPerpetual(only useful when isFastCreationEnabled)
 // ===============================================================================================
 // console.log(
 //   OperatorProxy.encodeFunctionData("createPerpetual", [
 //     '0xe44287a9ee676e92e7687c28bb30ac6b5cb80bb2', // just test pool
-//     '0x6ee936BdBD329063E8CE1d13F42eFEf912E85221', // BTC
+//     '0x6ee936BdBD329063E8CE1d13F42eFEf912E85221', // BTC oracle
 //     // imr          mmr            operatorfr        lpfr              rebate        penalty        keeper       insur         oi
 //     [toWei("0.04"), toWei("0.03"), toWei("0.00010"), toWei("0.00055"), toWei("0.2"), toWei("0.01"), toWei("10"), toWei("0.5"), toWei("3")],
 //     // alpha           beta1            beta2             frLimit        lev         maxClose       frFactor        defaultLev
