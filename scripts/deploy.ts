e7d3c70e768f0c8d7dcac14add9f746b9916f3a6
yashnaman <shahyashs145@gmail.com>
2021-11-04 14:28:46 +0530

    use weth from mainnet to be able to test it using mainnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -100,7 +100,9 @@ async function preset2(deployer, accounts) {
     console.log("WETH9 address", deployer.addressOf("WETH9"));
 
     // const usd = await deployer.getContractAt("CustomERC20", deployer.addressOf("CustomERC20"))
-    const weth = await deployer.getDeployedContract("WETH9");
+    let weth = await deployer.getDeployedContract("WETH9");
+    weth = weth.attach("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");//WETH9 on the mainnet
+    console.log("WETH9 address", weth.address);
 
     const poolCreator = await deployer.getDeployedContract("PoolCreator");
     console.log("creating liquidity pool");
